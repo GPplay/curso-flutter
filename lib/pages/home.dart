f5572d20ec05aa07c11987d436366e06d10a8c00
GPplay <gybramdali2002@hotmail.com>
2024-07-04 17:58:10 -0500

    metodo de _bandtile agregado

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,10 +18,23 @@ class _HomePageState extends State<HomePage>{
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('hola mundo')
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SuperHeroe', style: TextStyle(color: Colors.black87 ),),
+        backgroundColor: Colors.white,
       ),
+      body: ListView.builder(itemBuilder: (BuildContext context, int index) { 
+        return _bandtile(index);
+       },
+      )
     );
+  }
+
+  ListTile _bandtile(int index) {
+    return ListTile(
+        leading: CircleAvatar(
+          child: Text(bands[index].name?.substring(0, 2) ?? ""),
+        ),
+      );
   }
 }
