eaf4ac12afae45eef9778761862e18fb33a8d354
GPplay <gybramdali2002@hotmail.com>
2024-07-04 17:13:11 -0500

    agregando lista de bandas

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:practica_1/models/band.dart';
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  List<Band> bands = [
+    Band(id: '1', name: 'batman', vote: 55),
+    Band(id: '1', name: 'superman', vote: 25),
+    Band(id: '1', name: 'iroman', vote: 5),
+    Band(id: '1', name: 'megaman', vote: 15),
+  ];
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Text('Hola Mundo'),
-     ),
-   );
+        child: Text('hola mundo')
+      ),
+    );
   }
 }
